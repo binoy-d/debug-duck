6cc81744e14b9a5d6fe26efa904b2d9aae5d7d5d
Daniel Binoy <dbinoy15@gmail.com>
2020-02-01 13:22:37 -0800

    create bullet prefab

---
+++ Assets/Prefabs/BulletMover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMover : MonoBehaviour
+{
+    private Vector3 velocity; 
+    [SerializeField]
+    private float speed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocity = new Vector3(speed, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position+=velocity*Time.deltaTime*speed;
+    }
+}
