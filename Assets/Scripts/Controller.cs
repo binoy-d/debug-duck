906ca85af9ce7851d7a71f542ce3d3bb1c5f455c
mbusc1 <pianobuscemi@gmail.com>
2020-02-01 15:37:31 -0800

    Added controller script

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 1;
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        
+        Vector3 movement = new Vector3(horizontal,vertical);
+
+        transform.position = transform.position + (movement * Time.deltaTime * speed);
+    }
+}
