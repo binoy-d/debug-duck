e27c114e86e8331c26d8daae92d91c68e6f4786c
Daniel Binoy <dbinoy15@gmail.com>
2020-02-02 10:14:31 -0800

    emojihandler

---
+++ Assets/EmojiHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmojiHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private int health;
+    [SerializeField]
+    Sprite[] sprites = new Sprite[6];
+
+    void Start()
+    {
+        health = GameObject.Find("GameController").GetComponent<GameController>().getHealth();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<SpriteRenderer>().sprite = sprites[health-1];
+        health = GameObject.Find("GameController").GetComponent<GameController>().getHealth();
+    }
+}
