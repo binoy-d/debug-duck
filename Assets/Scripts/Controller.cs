f22edd0db2b32eb9e1bbee1dc90680033f99e5e1
cyntsao <cyntsao@users.noreply.github.com>
2020-02-01 22:01:24 -0800

    intro done, +debug controls []

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -13,28 +13,43 @@ public class Controller : MonoBehaviour
     
     private GameObject current;
 
+    private bool can_move_horizontal = false;
+    private bool can_move_vertical = false;
+    private float shoot_delay = 0f;
+    private float t = 0f;
 
-
-    // Update is called once per frame
     void Update()
     {
+        t -= Time.deltaTime;
+
         bool fire = Input.GetKeyDown("space");
 
-        if (fire)
+        if (fire && t <= 0f)
         {
             Vector3 pos = transform.position;
             pos[2] = 0;
             Shoot(pos);
+            t = shoot_delay;
         }
-        
+
+        Movement();
+    }
+
+    private void Movement()
+    {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        
-        Vector3 movement = new Vector3(horizontal,vertical,0);
+
+        if (!can_move_horizontal)
+            horizontal = 0;
+        if (!can_move_vertical)
+            vertical = 0;
+
+        Vector3 movement = new Vector3(horizontal, vertical, 0);
 
         Vector3 unmaxed = transform.position + (movement * Time.deltaTime * speed);
-        Vector3 maxed = new Vector3(Mathf.Min(Mathf.Max(unmaxed[0],-8.25f),8.25f),
-            Mathf.Min(Mathf.Max(unmaxed[1], -4.5f),4.5f),0);
+        Vector3 maxed = new Vector3(Mathf.Min(Mathf.Max(unmaxed[0], -8.25f), 8.25f),
+            Mathf.Min(Mathf.Max(unmaxed[1], -4.5f), 4.5f), 0);
 
         transform.position = maxed;
     }
@@ -42,5 +57,16 @@ public class Controller : MonoBehaviour
     private void Shoot(Vector3 t)
     {
         current = Instantiate(_bullet,t,Quaternion.identity);
+    }
+
+    public void SetCanMove(bool a, bool b)
+    {
+        can_move_horizontal = a;
+        can_move_vertical = b;
+    }
+
+    public void SetShootDelay(float d)
+    {
+        shoot_delay = d;
     }
 }
