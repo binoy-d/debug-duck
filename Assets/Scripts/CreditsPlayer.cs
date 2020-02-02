f869310074458ffaefc6155868938b9047475ac1
Evan Cheng <dr.frolicks@gmail.com>
2020-02-01 21:16:26 -0800

    credit work

---
+++ Assets/Scripts/CreditsPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+public class CreditsPlayer : MonoBehaviour
+{
+    [SerializeField]
+    float fadeTime, bannerPopUpDelay, bannerDisplayTime;
+    [SerializeField]
+    Image black, banner;  
+    public void Start()
+    {
+        StartCoroutine(creditsCoroutine()); 
+    }
+
+    public IEnumerator creditsCoroutine()
+    {
+        black.GetComponent<Image>().CrossFadeAlpha(0, 0, false);
+        black.GetComponent<Image>().CrossFadeAlpha(1, fadeTime, false);
+        yield return new WaitForSeconds(fadeTime + bannerPopUpDelay);
+        banner.enabled = true;
+        yield return new WaitForSeconds(bannerDisplayTime);
+        print("done"); 
+    }
+}
