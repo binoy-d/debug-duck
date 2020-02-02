af3bd12ed1f5de035fb1ef36c4372a8abb3165ef
mbusc1 <pianobuscemi@gmail.com>
2020-02-01 17:56:16 -0800

    Added bullet firing functionality updatign controller script

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,14 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEditor.UIElements;
 using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
     [SerializeField]
-    private float speed = 1;
+    private float speed = 10;
+    [SerializeField]
+    private GameObject _bullet;
 
-    
+    private GameObject current;
+
+
 
     // Update is called once per frame
     void Update()
@@ -16,8 +21,17 @@ public class Controller : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         
-        Vector3 movement = new Vector3(horizontal,vertical);
+        Vector3 movement = new Vector3(horizontal,vertical,0);
 
-        transform.position = transform.position + (movement * Time.deltaTime * speed);
+        transform.position += (movement * Time.deltaTime * speed);
+
+        bool fire = Input.GetKeyDown("space");
+        if (fire) { Shoot(transform.position); }
+        
+    }
+
+    private void Shoot(Vector3 t)
+    {
+        current = Instantiate(_bullet,t,Quaternion.identity);
     }
 }
