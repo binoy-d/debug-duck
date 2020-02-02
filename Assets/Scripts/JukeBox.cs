2d31708c9304b56d3d3990c40fad77aa9e2f587b
mbusc1 <pianobuscemi@gmail.com>
2020-02-02 13:22:03 -0800

    Added Jukebox

---
+++ Assets/Scripts/JukeBox.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JukeBox : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource _arcade;
+    [SerializeField]
+    private AudioSource _ominous;
+    [SerializeField]
+    private AudioSource _uplifting;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void PlayArcade()
+    {
+        _arcade.Play();
+    }
+    public void PlayOminous()
+    {
+        _ominous.Play();
+    }
+    public void PlayUplifting()
+    {
+        _uplifting.Play();
+    }
+    
+}
